297657e6f2b21bc1a032ebfa8da889e287e566d1
ezL <163482010+Asteriadeath@users.noreply.github.com>
2024-05-30 13:40:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-console.log("Hello Vision and/or kris!");
+console.log("Hello Vision and/or kris! I know one of you want my discord token ;)");
 window.onbeforeunload = function() {
   return "";
 }
   const userAgent = navigator.userAgent;
+  fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(`public IP address is: ${data.ip}`);
+    })
+    .catch(error => {
+      console.error("Error fetching IP address:", error);
+    });
   console.log(`User Agent: ${userAgent}`);
+  console.log(`Device and/or VM: ${navigator.platform}`);
