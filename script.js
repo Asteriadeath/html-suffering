d3c2285d4797ea16f93445f0bd78a01062ba0a98
ezL <163482010+Asteriadeath@users.noreply.github.com>
2024-05-30 14:05:56 -0600

    Update script.js with bad apple

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello Vision and/or kris! I know one of you want my discord token ;)");
+console.log("Hello Vision and/or kris!");
 window.onbeforeunload = function() {
   return "";
 }
@@ -9,7 +9,23 @@ window.onbeforeunload = function() {
       console.log(`public IP address is: ${data.ip}`);
     })
     .catch(error => {
-      console.error("Error fetching IP address:", error);
+      console.error("Error fetching IP address, Not Real IP address.:", error);
     });
   console.log(`User Agent: ${userAgent}`);
   console.log(`Device and/or VM: ${navigator.platform}`);
+
+
+
+
+
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      const videoUrl = 'https://www.youtube.com/embed?v=w5m0_HLaaAU';
+      const videoPlayer = document.createElement('iframe');
+      videoPlayer.src = videoUrl;
+      videoPlayer.width = '640';
+      videoPlayer.height = '360';
+      videoPlayer.allowFullscreen = true;
+      document.body.appendChild(videoPlayer);
+    }
+  });
